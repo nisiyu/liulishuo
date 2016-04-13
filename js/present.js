d00ecde85a488601ebc3570b7822c5f8a9ef9412
nisiyu <nisiyu91@gmail.com>
2016-04-14 01:19:19 +0800

    popup and close js code

---
+++ js/present.js
@@ -0,0 +1,10 @@
+var gift = {
+    popup:function() {
+        $("div.mask").css({"display": "block"});
+        $("div.present_dialog").css({"display": "block"});
+    },
+    close:function() {
+        $("div.mask").css({"display": "none"});
+        $("div.present_dialog").css({"display": "none"});
+    }
+}
