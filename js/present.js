d93a7486e34fa7729179f33edadbb4d3db5a7fbe
nisiyu <nisiyu91@gmail.com>
2016-05-15 00:56:49 +0800

    淡入淡出

--- js/present.js
+++ js/present.js
@@ -1,10 +1,15 @@
 var gift = {
     popup:function() {
         $("div.mask").css({"display": "block"});
-        $("div.present_dialog").css({"display": "block"});
+        $("div.present_dialog").css({"opacity":"1"});
+        $("body").bind("touchmove",function(e){e.preventDefault();});
     },
     close:function() {
-        $("div.mask").css({"display": "none"});
-        $("div.present_dialog").css({"display": "none"});
+        $("div.present_dialog").css({"opacity":"0"});
+        setTimeout(function(){
+            $("div.mask").css({"display": "none"});
+        }, 300);
+
+        $("body").unbind("touchmove");
     }
 };
